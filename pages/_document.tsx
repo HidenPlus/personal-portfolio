fbe06696032231ff264c5375dc9a4652161d7de9
Anyel Rojas <aatuesta00@hotmail.com>
2022-09-25 18:12:00 -0500

    Change http fonts to secure

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,13 @@ import Document, { Html, Main, NextScript, Head } from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
 const stylesUrl = [
-  "http://fonts.cdnfonts.com/css/inter",
+  "https://fonts.cdnfonts.com/css/inter",
   "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap",
   "https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swaphttps://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swaphttps://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap",
   "https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap",
   "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap",
-  "http://fonts.cdnfonts.com/css/sf-mono",
-  "http://fonts.cdnfonts.com/css/calibre",
+  "https://fonts.cdnfonts.com/css/sf-mono",
+  "https://fonts.cdnfonts.com/css/calibre",
 ]
 
 class MyDocument extends Document<{
@@ -27,7 +27,7 @@ class MyDocument extends Document<{
     return (
       <Html lang="es">
         <title>Yojhan Atuesta</title>
-        <link rel="preconnect" href="http://fonts.cdnfonts.com" />
+        <link rel="preconnect" href="https://fonts.cdnfonts.com" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         {stylesUrl.map((url, index) => (
           <link key={index} rel="stylesheet" href={url} />
