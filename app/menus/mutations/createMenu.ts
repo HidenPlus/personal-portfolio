f5003e0edecac20eee223302460b30839533cb06
Anyel Rojas <aatuesta00@hotmail.com>
2022-09-25 17:53:55 -0500

    Fix typo createMenu

--- app/menus/mutations/createMenu.ts
+++ app/menus/mutations/createMenu.ts
@@ -3,7 +3,13 @@ import db from "db"
 import { z } from "zod"
 
 const CreateMenu = z.object({
-  name: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  title: z.string(),
+  description: z.string(),
+  titleEs: z.string(),
+  authorId: z.number(),
+  to: z.string(),
 })
 
 export default resolver.pipe(resolver.zod(CreateMenu), resolver.authorize(), async (input) => {
