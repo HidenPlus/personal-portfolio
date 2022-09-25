496b7b9dcaf40b454aee6e0946c56d458d2707df
Anyel Rojas <aatuesta00@hotmail.com>
2022-09-25 18:00:07 -0500

    Go back form login

--- app/projects/components/ProjectForm/ProjectForm.tsx
+++ app/projects/components/ProjectForm/ProjectForm.tsx
@@ -6,7 +6,7 @@ export { FORM_ERROR } from "app/core/components/Form"
 
 export function ProjectForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
-    <Form<S> {...props}>
+    <Form {...props}>
       <TextField name="name" label="Name" placeholder="Name" />
     </Form>
   )
